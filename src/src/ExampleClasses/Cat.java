4e1db08297ce096f50c6e0fe36aa9d051d7b47b0
Juhar <jcsong06@gmail.com>
2023-10-19 13:51:53 -0500

    added Cat methods and documentation

--- src/src/ExampleClasses/Cat.java
+++ src/src/ExampleClasses/Cat.java
@@ -1,16 +1,29 @@
 package ExampleClasses;
 
 public class Cat {
+    // FIELDS
     public String species;
     public String color;
     public int population;
 
+    // CONSTRUCTOR
     public Cat(String scientificName, String color, int population) {
         this.species = scientificName;
         this.color = color;
         this.population = population;
     }
 
+    // POSSIBLE METHODS
+    public int addPopulation(int num) {
+        return this.population + num; // note this doesn't actually modify the population, only returns the modified value
+    }
+
+    public String getSpecies() {
+        System.out.println(this.species);
+        return this.species;
+    }
+
+    // HOW TO CREATE OBJECTS
     public static void main(String[] args) {
         Cat lion = new Cat("Panthera leo", "Gold", 23000);
         Cat tabby = new Cat("Felis Catus", "Mixed", 3000000);
