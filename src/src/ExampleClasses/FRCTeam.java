76cd0b11b8645558bfa5a950ab2c780995d7c748
Juhar <jcsong06@gmail.com>
2023-10-19 14:16:47 -0500

    documentation

--- src/src/ExampleClasses/FRCTeam.java
+++ src/src/ExampleClasses/FRCTeam.java
@@ -1,18 +1,21 @@
 package ExampleClasses;
 
 public class FRCTeam {
+    // FIELDS
     int teamNumber;
     String teamName;
     int numOfMembers;
     boolean qualifiedToWorlds;
     double avgMatchScore;
 
+    // CONSTRUCTOR
     public FRCTeam(int teamId, String name, int memberNum) {
         this.teamNumber = teamId;
         this.teamName = name;
         this.numOfMembers = memberNum;
     }
 
+    // METHODS
     public int getTeamNumber() {
         return teamNumber;
     }
@@ -25,6 +28,7 @@ public class FRCTeam {
         return qualifiedToWorlds;
     }
 
+    // MAIN
     public static void main(String[] args) {
         FRCTeam ERROR3130 = new FRCTeam(3130, "ERROR", 32);
         System.out.println(ERROR3130.getNumOfMembers());
