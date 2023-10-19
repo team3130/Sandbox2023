2566d852c3bbb0540f26d6135eca00ccd79390c5
Juhar <jcsong06@gmail.com>
2023-10-19 14:12:50 -0500

    added fields

--- src/src/ExampleClasses/Monster.java
+++ src/src/ExampleClasses/Monster.java
@@ -1,4 +1,9 @@
 package ExampleClasses;
 
 public class Monster {
+    public int size;
+    public double weight;
+    public String species;
+    public boolean isDangerous;
+    public String name;
 }
