0eba06351e5cf789cb98f6765b597550a04d738d
GAV <102122425+TheFishyCoder@users.noreply.github.com>
2023-11-20 19:26:26 -0600

    push
    pushed

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,7 +4,7 @@ MAIN IS NEVER USED IN FRC
 HOWEVER, IT IS IMPORTANT IN REAL LIFE CODE SO IF YOU WANNA KNOW LOOK INTO IT
  */
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
