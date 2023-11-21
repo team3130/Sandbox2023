fbc12de1182e25b9531df501619ed2c4ec50ee0c
BenThomas2 <151589108+BenThomas2@users.noreply.github.com>
2023-11-20 20:21:51 -0600

    created bEN Class

---
+++ src/src/bEN.java
@@ -0,0 +1,13 @@
+public class bEN {
+
+    public static void main(String[] args){
+        System.out.println("Ben Says Hi");
+    }
+    public String lastName;
+    public String age;
+    public bEN(){
+        this.lastName = "Thomas";
+        this.age = "25";
+    }
+
+}
