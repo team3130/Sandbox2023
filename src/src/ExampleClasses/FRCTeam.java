988ead641339dc8018599444593c57491a91e951
IsaacCoolGuy09 <151589944+IsaacCoolGuy09@users.noreply.github.com>
2023-11-29 20:02:36 -0600

    idk
    i fell asleep

--- src/src/ExampleClasses/FRCTeam.java
+++ src/src/ExampleClasses/FRCTeam.java
@@ -28,9 +28,18 @@ public class FRCTeam {
         return qualifiedToWorlds;
     }
 
+    public String getTeamName() {
+        return teamName;
+    }
+    public void setTeamNumber(int newnumber){
+        this.teamNumber = newnumber;
+    }
     // MAIN
     public static void main(String[] args) {
         FRCTeam ERROR3130 = new FRCTeam(3130, "ERROR", 32);
         System.out.println(ERROR3130.getNumOfMembers());
+        System.out.println(ERROR3130.getTeamNumber());
+        ERROR3130.setTeamNumber(5);
+        System.out.println(ERROR3130.getTeamNumber());
     }
 }
