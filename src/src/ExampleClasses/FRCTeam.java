21996fc2b0f2459317493c510e596e0c7c291494
Coriander <145623681+CorianderSeed@users.noreply.github.com>
2023-11-29 20:02:59 -0600

    last lesson

--- src/src/ExampleClasses/FRCTeam.java
+++ src/src/ExampleClasses/FRCTeam.java
@@ -9,10 +9,11 @@ public class FRCTeam {
     double avgMatchScore;
 
     // CONSTRUCTOR
-    public FRCTeam(int teamId, String name, int memberNum) {
+    public FRCTeam(int teamId, String name, int memberNum, double avgMatchScore) {
         this.teamNumber = teamId;
         this.teamName = name;
         this.numOfMembers = memberNum;
+        this.avgMatchScore = avgMatchScore;
     }
 
     // METHODS
@@ -28,9 +29,27 @@ public class FRCTeam {
         return qualifiedToWorlds;
     }
 
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public double getAvgMatchScore() {
+        return avgMatchScore;
+    }
+
+    public void setTeamNumber(int newTeamNumber) {
+        this.teamNumber = newTeamNumber;
+    }
+
+    public void setNumOfMembers(int newNumOfMembers) {
+        this.numOfMembers = newNumOfMembers;
+    }
+
     // MAIN
     public static void main(String[] args) {
-        FRCTeam ERROR3130 = new FRCTeam(3130, "ERROR", 32);
+        FRCTeam ERROR3130 = new FRCTeam(3130, "ERROR", 32, 0.2);
+        System.out.println(ERROR3130.getNumOfMembers());
+        ERROR3130.setNumOfMembers(33);
         System.out.println(ERROR3130.getNumOfMembers());
     }
 }
