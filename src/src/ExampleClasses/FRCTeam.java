8c6b94aac30073cfc13c159f0fcdf4feaf2c5f51
TreyMichalski <151589943+TreyMichalski@users.noreply.github.com>
2023-11-29 20:02:30 -0600

    day 4 sdfsdfsd

--- src/src/ExampleClasses/FRCTeam.java
+++ src/src/ExampleClasses/FRCTeam.java
@@ -19,14 +19,27 @@ public class FRCTeam {
     public int getTeamNumber() {
         return teamNumber;
     }
+    public void setTeamNumber(int newteamnumber){
+        this.teamNumber = newteamnumber;
+    }
 
     public int getNumOfMembers() {
         return numOfMembers;
     }
-
+    public void setNumOfMembers(int newnummember){
+        this.numOfMembers = newnummember;
+    }
     public boolean isQualifiedToWorlds() {
         return qualifiedToWorlds;
     }
+    public void setQualifiedToWorlds(boolean newqualifiedtoworlds){
+        this.qualifiedToWorlds = newqualifiedtoworlds;
+    }
+    public double getAvgMatchScore(){ return avgMatchScore;}
+    public void setAvgMatchScore(double newavgmatchscore){
+        this.avgMatchScore = newavgmatchscore;
+    }
+
 
     // MAIN
     public static void main(String[] args) {
