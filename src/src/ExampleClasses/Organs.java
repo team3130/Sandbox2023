78eeaeb44ce5b653ccea77dd7dfaf0de78427b12
JohnMadson <151589946+JohnMadson@users.noreply.github.com>
2023-12-03 11:23:18 -0600

    coding assignment

--- src/src/ExampleClasses/Organs.java
+++ src/src/ExampleClasses/Organs.java
@@ -1,5 +1,4 @@
 package ExampleClasses;
-
 public class Organs {
 
     public int nutrients;                       // inst. vars #1-4
@@ -14,23 +13,18 @@ public class Organs {
         this.organType = whatOrganIsIt;
     }
 
-
-    public int getNutrients() {         // getter #1
-        return nutrients;
+    public void getNutrients() {         // getter #1
     }
 
-    public double getShock() {          // getter #2
-        return shock;
+    public void getShock() {          // getter #2
     }
 
-    public int setNutrients(int newNutrients) {     // setter #1
+    public void setNutrients(int newNutrients) {     // setter #1
         this.nutrients = newNutrients;
-        return newNutrients;
     }
 
-    public int setBlood(int newBloodCount) {         // setter #2
+    public void setBlood(int newBloodCount) {         // setter #2
         this.blood = newBloodCount;
-        return newBloodCount;
     }
 
     public void moreBlood(int addedBloodFlow) {        // random print method #1
@@ -43,16 +37,38 @@ public class Organs {
         System.out.println(ecoFriendly);
     }
 
-
-    public static void main(String[] args) {
-        Organs heart = new Organs( 7, 9.4, 17, "Heart" );
-        Organs lungs = new Organs( 5, 5.2, 11, "Lungs" );
-        Organs brain = new Organs( 19, 25.3, 51, "Brain" );
-        System.out.println(heart.blood);
-        System.out.println(brain.nutrients);
-        System.out.println(lungs.shock);
-        heart.lessPower(1.2);
-        lungs.moreBlood(2);
+    public void shocky(double morePower) {              //random print method #3
+        double newShock = shock + morePower;
+        System.out.println(newShock);
     }
-}
+
+    public double newShock;
+
+    public void newShocky(double changeShock) {            //if statement attempt
+        if (newShock <= (changeShock + shock)) {
+            System.out.println("moreShock");
+        } else {
+            System.out.println("lessShock");
+        }
+    }
+
+        public static void main (String[]args){
+            Organs heart = new Organs(7, 9.4, 17, "Heart");
+            Organs lungs = new Organs(5, 5.2, 11, "Lungs");
+            Organs brain = new Organs(19, 25.3, 51, "Brain");
+            System.out.println(heart.blood);
+            System.out.println(brain.nutrients);
+            System.out.println(lungs.shock);
+            brain.shocky(-1);
+            heart.lessPower(1.2);
+            lungs.moreBlood(2);
+            heart.newShocky(0);
+            heart.setBlood(6);
+            lungs.getNutrients();
+            brain.getShock();
+            lungs.setNutrients(3);
+        }
+    }
+
+
 
